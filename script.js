b0b2b475803c555fc4f45d925cae7e7e703c6998
nuclearcat <undernzty1@gmail.com>
2024-07-25 01:41:24 +0300

    1

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 // alert('Hello World!')
-let name = 'jon'
+let name = 'JonSmith';
+let age = 25, massage = 'Nice to see you';
+let copy = massage;
+alert(name +'\n'+ age +'\n');
+document.write (copy);
+console.log(copy);
+console.log(7**5);
